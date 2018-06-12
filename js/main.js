51667a0d5085c256934e110090b1a45f380f3091
Dmitry Petrenko <d1mapetrenko@mail.ru>
2018-06-12 23:52:57 +0300

    optimization #2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
     $('.pyramid path').hover(function(){
-        $(this).parent().addClass('active');
-    }, function(){
-        $(this).parent().removeClass('active');
+        $(this).parent().toggleClass('active');
     });
-
     $('.pyramid .content').hover(function(){
-        $(this).parent().parent().addClass('active');
-    }, function(){
-        $(this).parent().parent().removeClass('active');
+        $(this).parents('g').toggleClass('active');
     });
 });
